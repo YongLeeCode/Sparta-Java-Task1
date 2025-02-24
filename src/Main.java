91b24632bac88ee2bf9d8f3b34dcaa97ee5d322f
YongLeeCode <yonghwan311@gmail.com>
2025-02-24 22:23:45 +0900

    feat: 유저 객체 구현

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import user.User;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        User user = new User();
+        user.setAge(20);
+        user.setGender("Male");
+        user.setName("John");
+        System.out.printf("저는 %d살 %s입니다. %s이죠.", user.getAge(), user.getName(), user.getGender());
     }
 }
