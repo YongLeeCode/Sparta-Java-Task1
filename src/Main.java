2cf9b1ef0cc61d7126c1e78180bf0c968bf20bb0
YongLeeCode <yonghwan311@gmail.com>
2025-02-25 21:11:02 +0900

    fix: 프로젝트 초기화

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
-import user.User;
-
 public class Main {
     public static void main(String[] args) {
-        User user = new User();
-        user.setAge(20);
-        user.setGender("Male");
-        user.setName("John");
-        System.out.printf("저는 %d살 %s입니다. %s이죠.", user.getAge(), user.getName(), user.getGender());
+
     }
 }
