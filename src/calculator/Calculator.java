24a38416b61ec4d520b4d457f3e48764ca19ec20
YongLeeCode <yonghwan311@gmail.com>
2025-02-26 10:46:43 +0900

    feat: 계산 기능 구현

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,4 +1,31 @@
 package calculator;
 
 public class Calculator {
+    private int num1;
+    private int num2;
+
+    public Calculator(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public double add() {
+        return num1 + num2;
+    }
+
+    public double subtract() {
+        return num1 - num2;
+    }
+
+    public double multiply() {
+        return num1 * num2;
+    }
+
+    public double divide() {
+        if (num2 == 0) {
+            System.out.println("0으로 나눌 수 없습니다.");
+            return -1;
+        }
+        return (double) num1 / num2;
+    }
 }
