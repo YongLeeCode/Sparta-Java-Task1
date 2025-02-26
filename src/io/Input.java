e6fc29a6a8356ea7c9386843f08defe14c41dfa4
YongLeeCode <yonghwan311@gmail.com>
2025-02-26 18:01:52 +0900

    feat: 출력, 프린트 클래스 구현

--- src/io/Input.java
+++ src/io/Input.java
@@ -1,24 +1,53 @@
-package calculator.io;
+package io;
 
+import dto.FormulaDto;
+
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class Input {
-    private static Scanner scanner = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
 
-    public static double inputNumber() {
+    public static FormulaDto playNew() {
+        // input
         System.out.print("첫번째 숫자 입력: ");
-        if(scanner.hasNextInt()) {
-            return scanner.nextInt();
-        } else if(scanner.hasNextDouble()) {
-            return scanner.nextDouble();
-        } else {
-            System.out.println("정수 혹은 실수만 입력 가능합니다.");
-            return 0;
-        }
+        Number firstNumber = inputNumber();
+        System.out.print("두번째 숫자 입력: ");
+        Number secondNumber = inputNumber();
+        System.out.print("사용할 사칙연산 기호: ");
+        char operator = inputOperator();
+        return new FormulaDto(firstNumber, secondNumber, operator);
     }
 
-    public static char inputOperator() {
+    public static FormulaDto playContinue(double firstNumber) {
+        // input
+        System.out.println("계속 계산하겠습니다.");
+        System.out.printf("첫번째 숫자 : %s \n", formatDouble(firstNumber));
+        System.out.print("두번째 숫자 입력: ");
+        Number secondNumber = inputNumber();
+        System.out.print("사용할 사칙연산 기호: ");
+        char operator = inputOperator();
+        return new FormulaDto(firstNumber, secondNumber, operator);
+    }
+
+    private static Number inputNumber() {
+        try {
+            if (scanner.hasNextDouble()) {
+                return scanner.nextDouble();
+            } else if (scanner.hasNextInt()) {
+                return scanner.nextInt();
+            }
+        } catch (Exception e) {
+            System.out.println("정수 혹은 실수만 입력 가능합니다.");
+        }
+        return 0;
+    }
+
+    private static char inputOperator() {
         return scanner.next().charAt(0);
     }
 
+    private static String formatDouble(double value) {
+        return new BigDecimal(value).stripTrailingZeros().toPlainString();
+    }
 }
