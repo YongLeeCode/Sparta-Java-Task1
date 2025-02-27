323c13d23edabc8682ea40837f6b4da6b1e387fd
YongLeeCode <yonghwan311@gmail.com>
2025-02-27 21:03:03 +0900

    feat: 레포지토리 구현

--- src/repository/RecordRepository.java
+++ src/repository/RecordRepository.java
@@ -1,4 +1,14 @@
 package repository;
 
+import Entity.CalculatorRecord;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface RecordRepository {
+    List<CalculatorRecord> findAll();
+
+    void save(CalculatorRecord calculatorRecord);
+
+    Optional<CalculatorRecord> findById(int id);
 }
--- src/repository/RecordRepositoryImpl.java
+++ src/repository/RecordRepositoryImpl.java
@@ -1,4 +1,30 @@
 package repository;
 
-public class RecordRepositoryImpl {
+import Entity.CalculatorRecord;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class RecordRepositoryImpl implements RecordRepository {
+    private static List<CalculatorRecord> records = new ArrayList<>();
+
+    // findAll
+    public List<CalculatorRecord> findAll() {
+        return records;
+    }
+
+    // findOneById
+    public Optional<CalculatorRecord> findById(int id) {
+        return records.stream()
+                .filter((x) -> x.getId() == id)
+                .findFirst();
+    }
+
+    // addNew
+    public void save(CalculatorRecord calculatorRecord) {
+
+        records.add(calculatorRecord
+        );
+    }
 }
