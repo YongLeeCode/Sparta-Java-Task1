1090726bb4a2c0f9d4a2127c4b801bca9925ec3e
YongLeeCode <yonghwan311@gmail.com>
2025-02-28 21:24:47 +0900

    feat: 계산 메서드, 추가, 삭제, 조회 기능 서비스 구현

--- src/service/CalculatorService.java
+++ src/service/CalculatorService.java
@@ -1,4 +1,16 @@
 package service;
 
+import Entity.CalculatorRecord;
+import dto.RecordDto;
+
+import java.util.List;
+
 public interface CalculatorService {
+    List<RecordDto> findAll();
+
+    RecordDto save(RecordDto dto);
+
+    Double findPrevById(int id);
+
+    void deleteById(int id);
 }
