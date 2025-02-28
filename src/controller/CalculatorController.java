bb9764adc94bb03d2a740de4949451af2041ae30
YongLeeCode <yonghwan311@gmail.com>
2025-02-28 21:25:01 +0900

    feat: 계산 컨트롤러 구현

--- src/controller/CalculatorController.java
+++ src/controller/CalculatorController.java
@@ -1,4 +1,34 @@
 package controller;
 
+import dto.RecordDto;
+import service.CalculatorService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class CalculatorController {
+
+    private CalculatorService calculatorService;
+
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    public RecordDto addRecord(RecordDto dto) {
+        return calculatorService.save(dto);
+    }
+
+    public List<RecordDto> findAll() {
+        return calculatorService.findAll();
+    }
+
+    public Double findPrev(int id) {
+        return calculatorService.findPrevById(id);
+    }
+
+    public void deleteById(int id) {
+        calculatorService.deleteById(id);
+    }
+
 }
